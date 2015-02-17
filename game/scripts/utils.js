e31ee5de590414740704473ecc65ea3d5a1485e7
Zachary Yaro <zmyaro@gmail.com>
2015-02-17 13:07:31 -0500

    Fix Utils.raf not being bound to window

--- game/scripts/utils.js
+++ game/scripts/utils.js
@@ -5,9 +5,9 @@ Utils = {};
  * Credit to Paul Irish.
  * @param {Function} func - The function to run on the next animation frame.
  */
-Utils.raf = window.requestAnimationFrame ||
+Utils.raf = (window.requestAnimationFrame ||
 	window.webkitRequestAnimationFrame ||
 	window.mozRequestAnimationFrame ||
 	window.msRequestAnimationFrame ||
 	window.oRequestAnimationFrame ||
-	function (func) { setTimeout(func, 1 / 60); };
+	function (func) { setTimeout(func, 1 / 60); }).bind(window);
