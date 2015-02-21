ec86bcdd6b99c55f10e4b02ea779db7de600e19c
rdp1070 <rdp1070@rit.edu>
2015-02-20 23:50:09 -0500

    Add stubs for Wall.js as outlined in UML

---
+++ game/scripts/wall.js
@@ -0,0 +1,32 @@
+var Wall = (function(){
+	'use strict'
+	
+	/**
+	 * Initialize a new Wall.
+	 * @param {Number} x - The Wall's starting x-coordinate
+	 * @param {Number} y - The Wall's starting y-coordinate
+	 * @param {Number} width - The Wall's width
+	 * @param {Number} height - The Wall's height
+	 */
+	function Wall(x, y, width, height) {
+		// Private variables
+		this._x;
+		this._y;
+		this._width;
+		this._height;
+		
+	}
+
+	// TODO 
+	// Determine whether or not wall collision happens here or in game.js
+	
+	/**
+	* @param {CanvasRenderingContext2D} cxt - The context on which the wall is drawn
+	*/
+	function draw(cxt) {
+		
+		
+	}
+	
+	
+})();
