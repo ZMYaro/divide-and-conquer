05c048029989ac2e5ba16b316a8c5324180a944a
rdp1070 <rdp1070@rit.edu>
2015-02-23 11:20:46 -0500

    Add the Collision function to Wall

--- game/scripts/wall.js
+++ game/scripts/wall.js
@@ -17,23 +17,53 @@ var Wall = (function(){
 		
 	}
 	
+	
 	// Static Constants
 	/** {Color} DEFAULT_STROKE_COLOR - The Wall's default stroke color */
 	Wall.DEFAULT_STROKE_COLOR = new Color(0, 0, 0);
 	
+		
 	
-	// TODO 
-	// Determine whether or not wall collision happens here or in game.js
-	
-	/**
-	* @param {CanvasRenderingContext2D} cxt - The context on which the wall is drawn
-	*/
-	function draw(cxt) {
-		// Change the stroke style to the default color then draw the rectangle
-		cxt.strokeStyle = Wall.DEFAULT_STROKE_COLOR;
-		cxt.rect(this._x, this._y, this._width, this._height);
-		cxt.stroke();
-	}
+	Wall.prototype = {
+		
+		/**
+		* Test whether or not the rectangle is colliding with a circle
+		* @param {Number} x - the x position of the circle
+		* @param {Number} y - the y position of the circle
+		* @param {Number} radius - the radius of the circle colliding
+		* Return whether or not the circle collided with the wall
+		*/
+		isColliding: function (x, y, radius) {
+			// Calculate the distance between the two shapes
+			var distX = Math.abs(x - this.x - this.width/2);
+			var distX = Math.abs(y - this.y - this.height/2);
+			
+			// If the distance is greater than half of each shape they are too far
+			if (distX > (this.width/2 + radius)) { return false;}
+			if (distY > (this.height/2 + radius)) { return false;}
+			
+			// If the distance is less than half then they are colliding
+			if (distX <= (this.width/2)) { return true; } 
+			if (distY <= (this.height/2)) { return true; } 
+			
+			// Check the last case for the corners 
+			// Pythagorean formula for the lines from center to corners
+			var dx = distX-this.width/2;
+			var dy = distY-this.height/2;
+			return ( dx * dy + dy * dy <= (radius *  radius) );
+		},
+		
+		/**
+		* @param {CanvasRenderingContext2D} cxt - The context on which the wall is drawn
+		*/
+		draw: function(cxt) {
+			// Change the stroke style to the default color then draw the rectangle
+			cxt.strokeStyle = Wall.DEFAULT_STROKE_COLOR;
+			// Draw the actual rectangle
+			cxt.rect(this._x, this._y, this._width, this._height);
+			cxt.stroke();
+		}
+	};
 	
 	return Wall;
 })();
