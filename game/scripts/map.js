87994ebada40d9f43c09adefa592cf201dc4be78
rdp1070 <rdp1070@rit.edu>
2015-02-23 11:43:09 -0500

    Add map.js and a few of it's functions

---
+++ game/scripts/map.js
@@ -0,0 +1,30 @@
+var Map = (function(){
+	'use strict'
+	
+	function Map(cxt) {
+		// Public variables
+		this.cxt;
+		this.field; 
+		
+		// Private variables
+		this._walls = [];
+	}
+	
+	Map.prototype = {
+		
+		/**
+		* Draw the map on the canvas
+		* @param {CanvasRenderingContext2D} cxt - The context on which the map is drawn
+		*/
+		draw: function(cxt) {
+			
+			// Go through the list of walls and draw each one
+			this._walls.foreach( function (wall) {
+				wall.draw(cxt);
+			}, this);
+			
+		}
+	};	
+	
+	return Map;
+})();
